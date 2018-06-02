6ce0853b558aa892f0eeb13380d58cb7aea7e87c
adirahman <kodingku@github.com>
2018-06-02 14:51:43 +0700

    update file index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include 'navigation.php';
 
 <head>
   <!-- Title -->
-  <title>Home Default | | Unify - Responsive Website Template</title>
+  <title>Setda Kabupaten Bogor</title>
 
   <!-- Required Meta Tags Always Come First -->
   <meta charset="utf-8">
@@ -19,9 +19,9 @@ include 'navigation.php';
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="shortcut icon" href="assets/img/icon.png">
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet"> 
   <!-- CSS Global Compulsory -->
   <link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
   <!-- CSS Global Icons -->
@@ -46,12 +46,13 @@ include 'navigation.php';
   <link rel="stylesheet" href="assets/css/unify-globals.css">
 
   <!-- CSS Customization -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
   <link rel="stylesheet" href="assets/css/custom.css">
 </head>
 
 <body>
-  <main>
+  <main style="font-family: 'Space Mono', monospace;font-size: 14px;">
 
 
 
@@ -94,7 +95,7 @@ include 'navigation.php';
                   <ul class="hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling" id="nav-submenu--features" aria-labelledby="nav-link--features">
                    
                     <li class="dropdown-item ">
-                      <a class="nav-link" href="unify-main/shortcodes/headers/index.html">Bagian Umum</a>
+                      <a class="nav-link" href="?page=umum">Bagian Umum</a>
                     </li>
 
                      <li class="dropdown-item ">
@@ -149,7 +150,7 @@ include 'navigation.php';
                   <ul class="hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling" id="nav-submenu--features" aria-labelledby="nav-link--features">
                    
                     <li class="dropdown-item ">
-                      <a class="nav-link" href="unify-main/shortcodes/headers/index.html">Asisten Pemerintahan</a>
+                      <a class="nav-link" href="?page=aspem">Asisten Pemerintahan</a>
                     </li>
 
                      <li class="dropdown-item ">
@@ -179,215 +180,14 @@ include 'navigation.php';
     <?php echo $content; ?>
     
 
-    <!-- Footer -->
-    <div id="contacts-section" class="g-bg-black-opacity-0_9 g-color-white-opacity-0_8 g-py-60">
-      <div class="container">
-        <div class="row">
-          <!-- Footer Content -->
-          <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
-            <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
-              <h2 class="u-heading-v2__title h6 text-uppercase mb-0">About Us</h2>
-            </div>
-
-            <p>About Unify dolor sit amet, consectetur adipiscing elit. Maecenas eget nisl id libero tincidunt sodales.</p>
-          </div>
-          <!-- End Footer Content -->
-
-          <!-- Footer Content -->
-          <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
-            <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
-              <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Latest Posts</h2>
-            </div>
-
-            <article>
-              <h3 class="h6 g-mb-2">
-            <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Incredible template</a>
-          </h3>
-              <div class="small g-color-white-opacity-0_6">May 8, 2017</div>
-            </article>
-
-            <hr class="g-brd-white-opacity-0_1 g-my-10">
-
-            <article>
-              <h3 class="h6 g-mb-2">
-            <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">New features</a>
-          </h3>
-              <div class="small g-color-white-opacity-0_6">June 23, 2017</div>
-            </article>
-
-            <hr class="g-brd-white-opacity-0_1 g-my-10">
-
-            <article>
-              <h3 class="h6 g-mb-2">
-            <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">New terms and conditions</a>
-          </h3>
-              <div class="small g-color-white-opacity-0_6">September 15, 2017</div>
-            </article>
-          </div>
-          <!-- End Footer Content -->
-
-          <!-- Footer Content -->
-          <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
-            <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
-              <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Useful Links</h2>
-            </div>
-
-            <nav class="text-uppercase1">
-              <ul class="list-unstyled g-mt-minus-10 mb-0">
-                <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
-                  <h4 class="h6 g-pr-20 mb-0">
-                <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">About Us</a>
-                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
-              </h4>
-                </li>
-                <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
-                  <h4 class="h6 g-pr-20 mb-0">
-                <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Portfolio</a>
-                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
-              </h4>
-                </li>
-                <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
-                  <h4 class="h6 g-pr-20 mb-0">
-                <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Our Services</a>
-                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
-              </h4>
-                </li>
-                <li class="g-pos-rel g-brd-bottom g-brd-white-opacity-0_1 g-py-10">
-                  <h4 class="h6 g-pr-20 mb-0">
-                <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Latest Jobs</a>
-                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
-              </h4>
-                </li>
-                <li class="g-pos-rel g-py-10">
-                  <h4 class="h6 g-pr-20 mb-0">
-                <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Contact Us</a>
-                <i class="fa fa-angle-right g-absolute-centered--y g-right-0"></i>
-              </h4>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <!-- End Footer Content -->
-
-          <!-- Footer Content -->
-          <div class="col-lg-3 col-md-6">
-            <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
-              <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Our Contacts</h2>
-            </div>
-
-            <address class="g-bg-no-repeat g-font-size-12 mb-0" style="background-image: url(assets/img/maps/map2.png);">
-          <!-- Location -->
-          <div class="d-flex g-mb-20">
-            <div class="g-mr-10">
-              <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
-                <i class="fa fa-map-marker"></i>
-              </span>
-            </div>
-            <p class="mb-0">795 Folsom Ave, Suite 600, <br> San Francisco, CA 94107 795</p>
-          </div>
-          <!-- End Location -->
-
-          <!-- Phone -->
-          <div class="d-flex g-mb-20">
-            <div class="g-mr-10">
-              <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
-                <i class="fa fa-phone"></i>
-              </span>
-            </div>
-            <p class="mb-0">(+123) 456 7890 <br> (+123) 456 7891</p>
-          </div>
-          <!-- End Phone -->
-
-          <!-- Email and Website -->
-          <div class="d-flex g-mb-20">
-            <div class="g-mr-10">
-              <span class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-color-white-opacity-0_6">
-                <i class="fa fa-globe"></i>
-              </span>
-            </div>
-            <p class="mb-0">
-              <a class="g-color-white-opacity-0_8 g-color-white--hover" href="mailto:info@htmlstream.com">info@htmlstream.com</a>
-              <br>
-              <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">www.htmlstream.com</a>
-            </p>
-          </div>
-          <!-- End Email and Website -->
-        </address>
-          </div>
-          <!-- End Footer Content -->
-        </div>
-      </div>
-    </div>
-    <!-- End Footer -->
+    
 
     <!-- Copyright Footer -->
-    <footer class="g-bg-gray-dark-v1 g-color-white-opacity-0_8 g-py-20">
+    <footer class="g-bg-gray-dark-v1 g-color-white-opacity-0_8 g-py-20 text-center" >
       <div class="container">
-        <div class="row">
-          <div class="col-md-8 text-center text-md-left g-mb-10 g-mb-0--md">
-            <div class="d-lg-flex">
-              <small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md">2018 &copy; All Rights Reserved.</small>
-              <ul class="u-list-inline">
-                <li class="list-inline-item">
-                  <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Privacy Policy</a>
-                </li>
-                <li class="list-inline-item">
-                  <span>|</span>
-                </li>
-                <li class="list-inline-item">
-                  <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Terms of Use</a>
-                </li>
-                <li class="list-inline-item">
-                  <span>|</span>
-                </li>
-                <li class="list-inline-item">
-                  <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">License</a>
-                </li>
-                <li class="list-inline-item">
-                  <span>|</span>
-                </li>
-                <li class="list-inline-item">
-                  <a class="g-color-white-opacity-0_8 g-color-white--hover" href="#!">Support</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-md-4 align-self-center">
-            <ul class="list-inline text-center text-md-right mb-0">
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Facebook">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Skype">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-skype"></i>
-                </a>
-              </li>
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Linkedin">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </li>
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Pinterest">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-pinterest"></i>
-                </a>
-              </li>
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Twitter">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top" title="Dribbble">
-                <a href="#!" class="g-color-white-opacity-0_5 g-color-white--hover">
-                  <i class="fa fa-dribbble"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+       
+              <small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md ">2018 &copy; Siratris</small>
+   
       </div>
     </footer>
     <!-- End Copyright Footer -->
@@ -429,6 +229,9 @@ include 'navigation.php';
   <script src="assets/js/components/hs.go-to.js"></script>
 
   <!-- JS Customization -->
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js "></script>
+
   <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
   <script src="assets/js/custom.js"></script>
 
@@ -481,8 +284,11 @@ include 'navigation.php';
   </script>
    <script type="text/javascript">
         $(document).ready(function(){
+
+            $('#table_id').DataTable();
+
             $('.slider').bxSlider({
-              mode : 'vertical',
+              mode : 'horizontal',
               auto : true,
               touchEnabled : true,
               controls : false
